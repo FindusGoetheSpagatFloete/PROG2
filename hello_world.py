4fcf7099edf7cd71904b273e631bd6eafed29a1a
FindusGoetheSpagatFloete <91332413+FindusGoetheSpagatFloete@users.noreply.github.com>
2021-11-19 16:37:50 +0100

    erster Entwurf

--- hello_world.py
+++ hello_world.py
@@ -1,1 +1,19 @@
-print("hello World")
+from flask import Flask
+from flask import render_template
+
+app = Flask("Hello World")
+
+
+@app.route("/hello")
+def hello():
+    return render_template('index.html', name="Andri")
+
+@app.route("/test")
+def test():
+    return "passt!"
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5000)
+
+
