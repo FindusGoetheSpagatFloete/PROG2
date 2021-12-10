f35c0cf0b6f65737c671a492b18a82d330dd298f
FindusGoetheSpagatFloete <91332413+FindusGoetheSpagatFloete@users.noreply.github.com>
2021-12-10 16:25:35 +0100

    diverse aenderungen + erstes json integriert

--- hello_world.py
+++ hello_world.py
@@ -1,16 +1,36 @@
 from flask import Flask
 from flask import render_template
+from flask import request
+import json
 
 app = Flask("Hello World")
 
-
+@app.route("/")
 @app.route("/hello")
 def hello():
     return render_template('index.html', name="Andri")
 
-@app.route("/test")
+@app.route("/test", methods= ["GET", "POST"])
 def test():
-    return "passt!"
+
+    if request.method == "POST":
+        s = open("spielerdaten.json")
+        spielerdaten_list = json.load(s)
+        vorname = request.form["vorname"]
+        nachname = request.form["nachname"]
+        team = request.form["team"]
+        goals = request.form["goals"]
+        assists = request.form["assists"]
+        punkte = request.form["punkte"]
+
+
+        spielerdaten_list.append({"vorname": vorname, "nachname": nachname, "team": team, "goals": goals, "assists": assists,"punkte": punkte})
+
+        with open("spielerdaten.json", "w") as f:
+            json.dump(spielerdaten_list, f, indent=4, separators=(",", ":"), sort_keys=True)
+
+
+    return render_template('spieler_formular.html')
 
 
 if __name__ == "__main__":
