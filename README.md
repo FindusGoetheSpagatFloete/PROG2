d4b2f226eaba8d80406aac5c2567d941fa1f0e47
FindusGoetheSpagatFloete <91332413+FindusGoetheSpagatFloete@users.noreply.github.com>
2022-01-06 20:36:36 +0100

    Kommentare und Readme ergänzt

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # PROG2
+
+Verwendete programme und Sprachen:
+- HTML
+- Python
+- Bootstrap (CSS)
+- Flask
+- JSON
+
+##Pojekt Hockeytabellen:
+Im Projekt soll es möglich sein Hockeyteams und Spieler via Formular zu erfassen. 
+Die eingetragenen Daten werden anschliessend in einem dazugehörigen JSON-File gespeichert.
+Diese Spieler und Teams werden dann in Python in eine Tabelle konvertiert und in HTML wiedergegeben.
+In Python werden die Daten zudem sortiert (nach Punkten). Des Weiteren werden 
+noch zusätzliche Berechnungen gemacht. Diese Berechnungen werden in Standarttabellen nie gemacht, was mich nervt, deshalb habe ich sie hinzugefügt.
+Mit Punkte pro Spiel kann man sehen wie viele Punkte ein Team im Durchschnitt pro Spiel macht.
+Dies ist nützlich in Zeiten wie jetzt, da auf Grund von COVID-19 viele Teams eine verschiedene Anzahl gespielte Spiele hat.
+Durch diese Angabe in der Tabelle sieht man welches Team aktuell am Besten unterwegs ist.
+Bei der Punkteausbeute sieht man wie viel Prozent der maximal möglichen Punktzahl ein Team erreicht hat.
+Bei den Spielern ist die Zusatzberechnung ebenfalls Punkte pro Spiel. Also wie viele Tore und Assists zusammen 
+ein Spieler im Durchschnitt pro Spiel macht. Dies ist spannend wenn ein Spieler zum Beispiel verletzt ist.
+Dann erscheint er nie in einer Scorertabelle obwohl er in Sachen Punkte pro Spiel gut dabei wäre
+aber das er auf Grund der Verletzung Spiele verpasst hat, ist er in der Tabelle nicht zu seheh.
--- hello_world.py
+++ hello_world.py
@@ -9,9 +9,18 @@ app = Flask("Hello World")
 @app.route("/hello")
 def hello():
 
+#Diese Funktion öffnet das json-file teamdaten um anschliessend damit arbeiten zu können.
     s = open("teamdaten.json")
     teamdaten_list = json.load(s)
     teamliste = []
+
+#max_punkte = berechnet maximal mögliche punktzahl (gespielte spiele x #3 (max punktzahl in einem spiel)
+#punkteausbeute = max punkte / (erreichte punkte*100) um prozentzahl #zu erhalten scheint mir als schönere darstellung
+#punkte pro spiel = punkte / spiele
+#teamliste.append = tabelleneinträge für jedes team werden mittels #diesem befehl im for loop erstellt mit allen
+#berechneten angaben und importierten angaben aus json ergänzt.
+#sorted = so wird die tabelle sortiert und zwar nach dem 5ten element #(0-4) das sind die erreichten punkte.
+#reverse = sortiert die liste von oben nach unten
 
     for team in teamdaten_list:
         max_punkte = int(team["gespielte_spiele"])*3
@@ -23,6 +32,8 @@ def hello():
 
     teamliste = sorted(teamliste, key=lambda x: x[4], reverse=True)
 
+#funktioniert gleich wie oben nur sind die Daten verschieden. Punkte #pro Spiel werden gleich wie bei den Teams berechnet
+#alle Daten stammen aus dem json-file spielerdaten.
     s = open("spielerdaten.json")
     spielerdaten_list = json.load(s)
     spielerliste = []
@@ -35,15 +46,14 @@ def hello():
 
     spielerliste = sorted(spielerliste, key=lambda x: x[5], reverse=True)
 
-
-
-
-
+#Hier geben wir die Daten der Tabellen und berechungen ans HTML file index.html weiter
     return render_template('index.html', teamliste=teamliste, spielerliste=spielerliste)
 
 @app.route("/test", methods= ["GET", "POST"])
 def test():
 
+#hier holen wir die Daten für die Tabellen oben.
+#wir öfffnen das json-file spielerdaten fügen die neu eingegebenen daten hinzu und speichern es wieder neu ab. So bleibt es immer aktuell.
     if request.method == "POST":
         s = open("spielerdaten.json")
         spielerdaten_list = json.load(s)
@@ -65,6 +75,8 @@ def test():
 
 @app.route("/team_formular", methods= ["GET", "POST"])
 def team_formular():
+
+#funktioniert gleich wie oben nur fpr das json-file teamdaten
     if request.method == "POST":
         s = open("teamdaten.json")
         teamdaten_list = json.load(s)
